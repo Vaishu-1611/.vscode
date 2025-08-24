6177d539faf816c9b296a366f802d41934a445ab
Vaishu-1611 <vmhetre608@gmail.com>
2025-08-24 22:36:47 +0530

    Refactor code structure for improved readability and maintainability

---
+++ sorting/bubbleD.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int main()
+{
+    int arr[7]={6,4,2,3,8,9,1};
+    int n=7;
+    for (int i=0;i<n;i++)
+{
+    printf("%d ",arr[i]);
+}
+for (int i=0;i<=n-1;i++)
+{
+    for(int j=0;j<=n-2;j++)
+    {
+        if(arr[j]<arr[j+1])
+        {
+            int temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp;
+        }
+    }
+}
+printf("\n");
+for (int i=0;i<=n-1;i++)
+{
+    printf("%d ",arr[i]);
+}
+return 0;
+}
---
+++ sorting/selection.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+#include<limits.h>
+int main()
+{
+    int arr[7]={6,4,2,3,8,9,1};
+    int n=7;
+    for (int i=0;i<n;i++)
+{
+    printf("%d ",arr[i]);
+}
+for (int i=0;i<=n-1;i++)
+{
+    int min=INT_MAX;
+    int minidx=-1;
+    for(int j=i;j<=n-1;j++)
+    {
+        if(min>arr[j])
+        {
+            min=arr[j];
+            minidx=j;
+        }
+    }
+    int temp=arr[minidx];
+    arr[minidx]=arr[i];
+    arr[i]=temp;
+}
+printf("\n");
+for (int i=0;i<=n-1;i++)
+{
+    printf("%d ",arr[i]);
+}
+return 0;
+}
